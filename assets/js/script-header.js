7728d2a4ac72030846a4b55c4ab694df40d1eda7
Alix Pelletier <alixpelletierpro@gmail.com>
2020-08-17 16:33:58 +0200

    smartphone menu display

--- assets/js/script-header.js
+++ assets/js/script-header.js
@@ -3,6 +3,8 @@ document.querySelector('.acceptbutton').addEventListener('click', function(){
     document.querySelector('.covid-banner').style.display = "none";
 });
 
+// =======================================================================================
+
 // searchbar
 document.querySelector('.close-button').style.display = "none";
 document.querySelector('.pop-up-searchbar').style.display = "none";
@@ -12,27 +14,23 @@ document.querySelector('.searchbar-button').addEventListener('click', function()
     document.querySelector('.searchbar-button').style.display = "none";
 });
 
-
 document.querySelector('.close-button').addEventListener('click', function(){
     document.querySelector('.pop-up-searchbar').style.display = "none";
     document.querySelector('.searchbar-button').style.display = "block";
     document.querySelector('.close-button').style.display = "none";
 });
 
-// burger menu (for tablets and smartphones)
-<div class="burger-menu">
-  <button class="burger-open" type="button" name="button"><i class="fas fa-bars"></i></button>
-</div>
-<div class="menu">
-  <button class="burger-close" type="button" name="button"><i class="fas fa-times"></i></button>
-  <nav>
-    <ul>
-      <li>NOS ARTISANS</li>
-      <li>NOS PRODUITS</li>
-      <li>NOS ENGAGEMENTS</li>
-      <li>LE MEDIA</li>
-    </ul>
-  </nav>
-</div>
+// =======================================================================================
 
-document.querySelector('.burger-close').style.display = "none";
+// burger menu (for tablets and smartphones)
+
+document.querySelector('.smartphone-menu').style.display = "none";
+document.querySelector('.burger-open').addEventListener('click', function(){
+  document.querySelector('.smartphone-menu').style.display = "block";
+  document.querySelector('.burger-menu').style.display = "none";
+});
+
+document.querySelector('.burger-close').addEventListener('click', function(){
+  document.querySelector('.smartphone-menu').style.display = "none";
+  document.querySelector('.burger-menu').style.display = "block";
+})
