367039314f22b0608d31cfcdfeef2e52e9a1f7f0
delfdum <60744187+delfdum@users.noreply.github.com>
2020-08-29 15:29:20 +0200

    page admin produit

---
+++ src/admin/NouveauProduit.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="">
+    <h2>Ajouter un produit</h2>
+    <form>
+        <label for="">name</label>
+        <input type="text" v-model="name"/> <br>
+
+        <label for="artisan-select">Choisir l'artisan:</label>
+          <select name="artisans" v-model="artisan_id" >
+            <option v-for="artisan in artisans" :key="artisan.id" :value="artisan.id" > {{artisan.name}}</option>
+          </select>  <br>
+
+        <label for="">category</label>
+        <input type="text" v-model="category"/> <br>
+
+        <label for="">type</label>
+        <input type="text" v-model="type"/> <br>
+
+        <label for="">matériel</label>
+        <input type="text" v-model="materials"/> <br>
+
+        <!-- thumbnail -->
+        <!-- photo -->
+
+        <label for="dimensions">dimensions</label>
+        <input type="text" v-model="dimensions"/> <br>
+
+        <label for="origine">origine</label>
+        <input type="text" v-model="origine"/> <br>
+
+        <label for="longPrez">présentation</label>
+        <input type="text" v-model="longPrez"/> <br>
+
+        <label for="">produit du mois</label>
+        <input type="text" v-model="produitDuMois"/> <br>
+
+        <label for="artisanDuMois">artisan du mois</label>
+        <input type="text" v-model="artisanDuMois"/> <br>
+
+        <button type="button" @click="postProduit">Envoyer</button>
+    </form>
+ <!--la réponse du formulaire ci-dessous si ça a été validé:  -->
+    <p v-if="success">Le produit a bien été enregistré</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NouveauProduit",
+  data() {
+    return {
+      artisans: "",
+       name: "",
+       artisan_id:"",
+       category: "",
+       type: "",
+       materials: "",
+       thumbnail: "",
+       photo: "",
+       dimensions: "",
+       origine: "",
+       longPrez: "",     
+       produitDuMois: false,
+       artisanDuMois: false,
+       success: false,
+    }
+  },
+
+    mounted() {
+     const axios = require("axios");
+     axios.get('http://localhost:3000/artisans')
+     .then(response => (this.artisans = response.data))
+   },
+
+  methods: {
+    postProduit() {
+      const axios = require('axios');
+  //dans axios.post{2 arguments 1/ le chemin URL, 2/ le body, c'est les infos qu'on envoie à l'API et c'est toujours un objet}
+      axios.post(`http://localhost:3000/produits/nouveau`, {
+          name: this.name,
+          artisan_id: this.artisan_id,
+          category: this.category,
+          type: this.type,
+          materials: this.materials,
+          thumbnail: this.thumbnail,
+          photo: this.photo,
+          dimensions: this.dimensions,
+          origine: this.origine,
+          longPrez: this.longPrez,
+          produitDuMois: this.produitDuMois,
+          artisanDuMois: this.artisanDuMois,
+      })
+      .then(() => {
+        this.name = "";
+        this.artisan_id = "";
+        this.category= "";
+        this.type= "";
+        this.materials= "";
+        this.thumbnail = "";
+        this.photo = "";
+        this.dimensions = "";
+        this.origine = "";
+        this.longPrez = "";
+        this.produitDuMois = "";
+        this.artisanDuMois = "";
+        this.success = true;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  }
+}
+</script>
