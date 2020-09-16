67fb977b43fc1ad840503e8f1ca1b53ed1193e15
delfdum <60744187+delfdum@users.noreply.github.com>
2020-09-16 15:34:05 +0200

    ajout put produit et modif

--- serveur-ethically/index.js
+++ serveur-ethically/index.js
@@ -1,66 +1,68 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const app = express();
-// create connection à la bdd mysql:
+
+const mysql = require("mysql2");
+
 const db = mysql.createConnection({
     host:'localhost',
     user: 'root',
-    password: 'MOUCHETTE',
-    database: 'new_schema_alix'
+    password: 'root',
+    database: 'ethically',
+    port: 8889,
+
 });
-
-// ALIX ALIX ALIX ALIX ALIX ALIX ALIX ALIX ALIX ALIX ALIX 
-// ci-dessus dans password, mets le tien
-
-
-
-
 
 // Connection à la bdd:
 db.connect((err) => {
     if(err){
         throw err;
     }
-    console.log('MySql Connected');
+    console.log('MySql is Connected');
 });
+
+
+const app = express();
+
+  
 // Utilisation du middleware cors pour éviter les problèmes de Same Origin
 app.use(cors());
 
 // support parsing of application/json type post data
+
 app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /////////////////////////////////////////////////////////////////////////////
-//requête GET pour afficher tous les produits:
+//requête GET pour afficher tous les produits par id décroissant:
 app.get('/produits', (req, res) => {
-    let sql = 'SELECT * FROM produits';
+    let sql = 'SELECT * FROM produits ORDER BY id DESC';
     db.query(sql, (err, results) => {
         if(err) throw err;
         res.status(200).send(results);
     })
 })
 //requête GET pour afficher un produit:
-app.get('/produit/:id', (req, res) => {
+app.get('/produitSelect/:id', (req, res) => {
     let sql = 'SELECT * FROM produits WHERE id=' + req.params.id;
     db.query(sql, (err, results) => {
         if(err) throw err;
         res.status(200).send(results);
     })
 })
-//requête GET pour afficher tous les artisans:
+
+//requête GET pour afficher tous les artisans par id décroissant:
 app.get('/artisans', (req, res) => {
-    let sql = 'SELECT * FROM artisans';
+    let sql = 'SELECT * FROM artisans ORDER BY id DESC';
     db.query(sql, (err, results) => {
         if(err) throw err;
         res.status(200).send(results);
     })
 })
 //requête GET pour afficher un artisan:
-app.get('/artisan/:id', (req, res) => {
+app.get('/artisanSelect/:id', (req, res) => {
     let sql = 'SELECT * FROM artisans WHERE id=' + req.params.id;
     db.query(sql, (err, results) => {
         if(err) throw err;
@@ -68,9 +70,64 @@ app.get('/artisan/:id', (req, res) => {
     })
 })
 //requête GET pour afficher LE produit de l'artisan:
-//PAS TRES SURE ENCORE DONC TU PEUX COMMENTER POUR VOIR SI LE RESTE FONCTIONNE
 app.get('/produitsArtisan/:id', (req, res) => {
     let sql = 'SELECT * FROM produits WHERE artisan_id=' + req.params.id;
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.status(200).send(results);
+    })
+})
+
+////REQUETE GET POUR PAGE ACCUEIL////////////////////////////////////////////////////////
+// GET POUR AFFICHER 8 PRODUITS MAXI QUI ONT été sélectionné par firstPage
+app.get('/produitsFirstpage', (req, res) => {
+    let sql = 'SELECT * FROM produits WHERE firstPage = "true" LIMIT 8';
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.status(200).send(results);
+    })
+})
+// GET POUR AFFICHER 8 ARTISANS MAXI QUI ONT été sélectionné par firstPage
+app.get('/artisansFirstpage', (req, res) => {
+    let sql = 'SELECT * FROM artisans WHERE firstPage = "true" LIMIT 8';
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.status(200).send(results);
+    })
+})
+
+//requête GET pour afficher l' artisan du mois:
+// app.get('/artisanDuMois/:id', (req, res) => {
+//     let sql = 'SELECT * FROM artisans WHERE id=' + req.params.id + 'AND artisanDuMois="true" LIMIT 1 ';
+//     db.query(sql, (err, results) => {
+//         if(err) throw err;
+//         res.status(200).send(results);
+//     })
+// })
+
+
+//requête GET pour afficher 4 produits de l'artisan du mois:
+app.get('/produitsArtisanDuMois/:id', (req, res) => {
+    let sql = 'SELECT * FROM produits WHERE artisan_id=' + req.params.id  ;
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.status(200).send(results);
+    })
+})
+
+// requête GET pour sélectionner les produits par categorie
+app.get('/produitsCategory/:category', (req, res) => {
+    let sql = "SELECT * FROM produits WHERE category =" + "'" + req.params.category + "'";
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.status(200).send(result);
+    })
+  });
+
+
+//requête GET pour afficher l'artisan correspondant à un produit
+app.get('/artisanDuProduit/:id', (req, res) => {
+    let sql = 'SELECT nom, artisan_id FROM artiste, produits WHERE artiste.id_artiste = produits.artisan_id AND artisan_id =' + req.params.id  ;
     db.query(sql, (err, results) => {
         if(err) throw err;
         res.status(200).send(results);
@@ -102,8 +159,7 @@ app.post('/produits/nouveau', (req, res) => {
                                     dimensions, 
                                     origine, 
                                     longPrez, 
-                                    produitDuMois, 
-                                    artisanDuMois) 
+                                    firstPage) 
                 VALUES ('${req.body.name}',
                         '${req.body.artisan_id}', 
                         '${req.body.category}',
@@ -113,9 +169,8 @@ app.post('/produits/nouveau', (req, res) => {
                         '${req.body.photo}', 
                         '${req.body.dimensions}',
                         '${req.body.origine}', 
-                        '${req.body.longPrez}', 
-                        '${req.body.produitDuMois}', 
-                        '${req.body.artisanDuMois}' )`;
+                        '${req.body.longPrez}',
+                        '${req.body.firstPage}')`; 
     db.query(sql, (err, results) => {
         if(err) throw err;
         res.status(200).send(results);
@@ -133,32 +188,54 @@ app.delete('/artisandelete/:id', (req, res) => {
     })
 })
 
-
+//requête DELETE pour supprimer un produit:
+app.delete('/produitdelete/:id', (req, res) => {
+    let sql = 'DELETE FROM produits WHERE id=' + req.params.id;
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.status(200).send(results);
+    })
+})
 /////////////////////UPDATE////////////////////////////////////////////////
-///requête UPDATE pour un artisan    
-     
+
+///requête UPDATE pour un artisan         
 app.put('/changeArtisan/:id', (req, res) => {
-    let sql =  `UPDATE artisans SET                  
-                        name = '${req.body.name}' , 
-                        presentation = '${req.body.presentation}', 
-                        thumbnail = '${req.body.thumbnail}', 
-                        photo = '${req.body.photo}', 
-                        firstPage = ${req.body.firstPage}, 
-                        artisanDuMois = ${req.body.artisanDuMois}
+    let sql =  `UPDATE artisans SET                                     
+                        name = '${req.body.name}',
+                        presentation = '${req.body.presentation}',
+                        photo = '${req.body.photo}',            
+                        origine = '${req.body.origine}',
+                        firstPage = '${req.body.firstPage}',
+                        artisanDuMois = '${req.body.artisanDuMois}'
+                                   
     WHERE id=` + req.params.id;
+  
     db.query(sql, (err, results) => {
         if(err) throw err;
         res.status(200).send(results);
     })
 })
 
-
-
-
-
-
-
-
+///requête UPDATE pour un produit
+app.put('/changeProduit/:id', (req, res) => {
+    let sql =  `UPDATE produits SET
+                        name = '${req.body.name}',
+                        artisan_id = '${req.body.artisan_id}',
+                        category = '${req.body.category}',
+                        type = '${req.body.type}',
+                        materials = '${req.body.materials}',                    
+                        photo = '${req.body.photo}',
+                        dimensions = '${req.body.dimensions}',
+                        origine = '${req.body.origine}',
+                        longPrez = '${req.body.longPrez}',
+                        firstPage = ${req.body.firstPage},
+                        produitDuMois = '${req.body.produitDuMois}'
+    WHERE id=` + req.params.id;
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.status(200).send(results);
+    })
+})
 
 
 
